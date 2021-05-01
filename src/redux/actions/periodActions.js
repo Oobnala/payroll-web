db85610b287dc028c8ae016dd7ad6d2f38d05465
Alan Boo <alanboo@Alans-MacBook-Pro.local>
2021-04-30 17:18:41 -0700

    Fix dates in header of period table. Created handlers and hooks to handle employee input values

--- src/redux/actions/periodActions.js
+++ src/redux/actions/periodActions.js
@@ -1,5 +1,5 @@
 import server from '../../api/server';
-import { GET_ALL_PAY_PERIODS } from './types';
+import { GET_ALL_PAY_PERIODS, GET_DATES } from './types';
 
 export const getPayPeriods = () => async (dispatch) => {
   await server
@@ -10,4 +10,13 @@ export const getPayPeriods = () => async (dispatch) => {
     .catch((err) => {
       console.log(err);
     });
+};
+
+export const getDates = () => async (dispatch) => {
+  await server
+    .get('periods')
+    .then((response) => {
+      dispatch({ type: GET_DATES, payload: response.data });
+    })
+    .catch((err) => {});
 };
