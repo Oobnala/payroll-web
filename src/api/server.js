2b6d290164c4fe2f253efb6eace247e07d0f5791
Monty Saengsavang <monty@Montys-MacBook-Pro-2.local>
2021-05-15 04:02:42 -0400

    oops port 8000

--- src/api/server.js
+++ src/api/server.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const server = axios.create({
-  baseURL: (process.env.NODE_ENV === 'production') ? "http://api.chaoprayanovato.com/" : "localhost:8080/",
+  baseURL: (process.env.NODE_ENV === 'production') ? "http://api.chaoprayanovato.com/" : "localhost:8000/",
   timeout: 3000,
   headers: {
     'content-type': 'application/json',
